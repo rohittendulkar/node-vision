bb48c1dc3fee9ae9e4137df0ecf535b62e3d169f
rohittendulkar <vrekedit00@gmail.com>
2022-09-15 12:31:25 +0530

    Image Label Detection through AWS Rekognition

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 // middleware to handle uploaded files
-var fileUpload = require('express-fileupload');
+var fileUpload = require("express-fileupload");
 
-var indexRouter = require('./routes/index');
-var visionRouter = require('./routes/vision');
+var indexRouter = require("./routes/index");
+var visionRouter = require("./routes/vision");
+require("dotenv").config();
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Limit file upload to 5 MB
-app.use(fileUpload({
-  limits: { 
-    fileSize: 5 * 1024 * 1024 
-  },
-  limitHandler: (req, res, next) => {
-    res.status(413).json({
-      'error': 'Max allowed filesize is 5 MB'
-    })
-  }
-}));
-app.use(logger('dev'));
+app.use(
+	fileUpload({
+		limits: {
+			fileSize: 5 * 1024 * 1024,
+		},
+		limitHandler: (req, res, next) => {
+			res.status(413).json({
+				error: "Max allowed filesize is 5 MB",
+			});
+		},
+	})
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/vision', visionRouter); // Check out the comments in this router for more details!
+app.use("/", indexRouter);
+app.use("/vision", visionRouter); // Check out the comments in this router for more details!
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,60 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var AWS = require("aws-sdk");
 
-router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+router.post("/classify", function (req, res, next) {
+	// Your code starts here //
 
-  // Your code starts here //
+	//Throw error if no image upload
+	if (!req.files || req.files.length == 0) {
+		res.status(400).json({
+			error: "You need to upload an image",
+		});
+	}
 
-  // Your code ends here //
+	//Check file data
+	console.log(req.files.file);
 
-  res.json({
-    "labels": response
-  });
+	const file = req.files.file;
+	const buffer = file.data;
+
+	console.log(buffer);
+
+	//AWS config
+	const config = new AWS.Config({
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+		region: process.env.AWS_REGION,
+	});
+
+	console.log(config);
+
+	const client = new AWS.Rekognition();
+
+	var params = {
+		Image: {
+			Bytes: buffer,
+		},
+	};
+
+	client.detectLabels(params, (err, response) => {
+		if (err) {
+			console.log(err, err.stack); // if an error occurred
+
+			res.status(500).json({
+				error: "Server side error",
+			});
+		} else {
+			// console.log(`Successfully Detected labels for ${photo}`);
+			console.log(response);
+			res.status(200).json({
+				labels: response.Labels.map((label) => {
+					return label.Name;
+				}),
+			});
+		}
+	});
+	// Your code ends here //
 });
 
 module.exports = router;
